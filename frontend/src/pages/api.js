39e3031fa5edd446c2e6a1fdb07faf749a48fea2
rmurph07 <rmurph07@syr.edu>
2025-02-11 12:19:07 -0500

    making sqlite work

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -7,12 +7,26 @@ const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
 });
 
+// Fetch all customers
 export const fetchCustomers = async () => {
-  const response = await axiosInstance.get('/customers');
+  const response = await axiosInstance.get("/customers");
   return response.data;
 };
 
+// Fetch all vehicles
 export const fetchVehicles = async () => {
-  const response = await axiosInstance.get('/vehicles');
+  const response = await axiosInstance.get("/vehicles");
   return response.data;
+};
+
+// Fetch total customers count
+export const fetchCustomersCount = async () => {
+  const response = await axiosInstance.get("/customers/count");
+  return response.data; // assuming it's just a number
+};
+
+// Fetch total vehicles count
+export const fetchVehiclesCount = async () => {
+  const response = await axiosInstance.get("/vehicles/count");
+  return response.data; // assuming it's just a number
 };
